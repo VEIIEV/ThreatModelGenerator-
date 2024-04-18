fca137e965028793afc278c38dda0f3b43d196bd
veiiev <worldloflwarcraft@mail.ru>
2024-04-18 18:31:09 +0400

    в html  отображаются закрепленные на данный момент ответственные лица

--- ThreatModelGenerator/settings.py
+++ ThreatModelGenerator/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 import paramiko
+from django.conf.global_settings import STATICFILES_DIRS
 from sshtunnel import SSHTunnelForwarder
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -175,6 +176,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
